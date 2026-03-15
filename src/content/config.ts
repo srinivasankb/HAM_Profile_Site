2df7ea20c7c7d64a650cc3a31fbb25e1a4f22353
Srinivasan KB <srinivasan.kb+DFH@candescent.com>
2026-03-15 21:40:18 +0530

    Fix deployment issues
    Fix deployment issues

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogs = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        date: z.date(),
+        author: z.string(),
+        description: z.string(),
+        tags: z.array(z.string()),
+        image: z.string().optional(),
+    }),
+});
+
+export const collections = {
+    'blogs': blogs,
+};
